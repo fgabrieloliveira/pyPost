a2ea0c20134a4501a99856aaa11ea0bd289e1bf1
gabriel <inexistentek@protonmail.com>
2021-06-26 20:36:51 -0300

    models improvement

--- main/models.py
+++ main/models.py
@@ -1,7 +1,9 @@
 
 from django.db import models
-from django.db.models.deletion import SET_DEFAULT
+from django.db.models.deletion import CASCADE, SET_DEFAULT
 from  django.utils import timezone
+from django.contrib.auth.models import User
+from myproject.settings import MEDIA_URL
 
 class Reporters(models.Model):
     name = models.CharField(max_length=20)
@@ -15,10 +17,10 @@ class Reporters(models.Model):
 class Articles(models.Model):
     title = models.CharField(max_length=20)
     headline = models.TextField(max_length=50)
-    text = models.TextField(max_length=1000)
-    author = models.ForeignKey(Reporters, on_delete=SET_DEFAULT, default='desconhecido')
+    text = models.TextField(max_length=100000)
+    author = models.ForeignKey(User, on_delete=CASCADE)
     pub_date = models.DateField(default=timezone.now)
-    image = models.ImageField(default='static/images/default.jpeg')
+    image = models.ImageField(default='static/images/default.jpeg', upload_to=MEDIA_URL)
     
     def __str__(self):
         return self.title
